53aa8b6a0c8f1954e320c0dd4c474641c0b50dab
DESKTOP-0S2LP1S\naqi <naqisyed@gmail.com>
2022-12-27 15:28:21 -0600

    adding more variables

--- server/config/index.js
+++ server/config/index.js
@@ -2,12 +2,20 @@
 
 module.exports = {
   default: {
-    GOOGLE_OAUTH_REDIRECT_URI: process.env.GOOGLE_OAUTH_REDIRECT_URI || 'http://localhost:1337/strapi-plugin-sso/google/callback',
-    GOOGLE_GSUITE_HD: process.env.GOOGLE_GSUITE_HD || '',
-    GOOGLE_ALIAS: process.env.GOOGLE_ALIAS || '',
 
-    COGNITO_OAUTH_REDIRECT_URI: process.env.COGNITO_OAUTH_REDIRECT_URI || 'http://localhost:1337/strapi-plugin-sso/cognito/callback',
-    COGNITO_OAUTH_REGION: process.env.COGNITO_OAUTH_REGION || 'us-east-1',
+    // Google
+    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_REDIRECT_URI || '[Client ID created in GCP]',
+    GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_GSUITE_HD || '[Client Secret created in GCP]',
+    GOOGLE_OAUTH_REDIRECT_URI: process.env.GOOGLE_OAUTH_REDIRECT_URI || 'http://localhost:1337/strapi-plugin-sso/google/callback', // URI after successful login
+    GOOGLE_ALIAS: process.env.GOOGLE_ALIAS || '', // Gmail Aliases
+    GOOGLE_GSUITE_HD: process.env.GOOGLE_GSUITE_HD || '', // G Suite Primary Domain
+
+    // Cognito
+    COGNITO_OAUTH_CLIENT_ID: process.env.COGNITO_OAUTH_CLIENT_ID || '[Client ID created in AWS Cognito]',
+    COGNITO_OAUTH_CLIENT_SECRET: process.env.COGNITO_OAUTH_CLIENT_SECRET || '[Client Secret created in AWS Cognito]',
+    COGNITO_OAUTH_DOMAIN: process.env.COGNITO_OAUTH_DOMAIN || '[OAuth Domain created in AWS Cognito]',
+    COGNITO_OAUTH_REDIRECT_URI: process.env.COGNITO_OAUTH_REDIRECT_URI || 'http://localhost:1337/strapi-plugin-sso/cognito/callback', //  // URI after successful login
+    COGNITO_OAUTH_REGION:  process.env.COGNITO_OAUTH_REGION || 'us-east-1', // AWS Cognito Region
   },
   validator() {
   },
